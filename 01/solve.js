2d6409574a8b9db41ef82d549f25e4cce4e38034
Anders Svensson <andess86@gmail.com>
2021-12-09 15:30:28 +0100

    add day 01 part 2

--- 01/solve.js
+++ 01/solve.js
@@ -14,3 +14,25 @@ measurements.map((measurement, i) => {
 console.log(
   "Number of measurements larger than previous one is " + nrOfLargerMeasurements
 );
+
+let sumOfSlidingWindow = [];
+measurements.map((measurement, i) => {
+  if (measurements[i + 1] && measurements[i + 2]) {
+    sumOfSlidingWindow.push(
+      measurement + measurements[i + 1] + measurements[i + 2]
+    );
+  }
+});
+
+let nrOfLargerMeasurementsSlidingWindow = 0;
+
+sumOfSlidingWindow.map((measurement, i) => {
+  if (measurement > sumOfSlidingWindow[i - 1]) {
+    nrOfLargerMeasurementsSlidingWindow++;
+  }
+});
+
+console.log(
+  "Number of measurements within sliding window larger than previous one is " +
+    nrOfLargerMeasurementsSlidingWindow
+);

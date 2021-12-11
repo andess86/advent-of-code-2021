725c8975a7b9e6ff23717319f8efede0a9b7d27f
Anders Svensson <andess86@gmail.com>
2021-12-11 08:32:15 +0100

    add day 02 part 2

--- 02/solve.js
+++ 02/solve.js
@@ -7,37 +7,61 @@ const input = fs.readFileSync("input.txt", "utf-8", (input, err) => {
 
 // Data massage
 const commands = input.split("\n");
+
 let commandsArray = commands.map((command) => {
   let arr = [];
-  let t = command.split(" ");
-  let c = t[0];
-  let number = t[1];
+  let [c, number] = command.split(" ");
   arr.push(c, parseInt(number));
   return arr;
 });
 
-// World position
-let horizontalPos = 0;
-let depth = 0;
-
 // Do stuff
-const moveAroundSubmarine = (commands) => {
-  commands.map((command) => {
-    switch (command[0]) {
-      case "forward":
-        horizontalPos = horizontalPos + command[1];
-        break;
-      case "up":
-        depth = depth - command[1];
-        break;
-      case "down":
-        depth = depth + command[1];
-        break;
+const moveAroundSubmarine = (commands, part2 = false) => {
+  // World position
+  let horizontalPos = 0;
+  let depth = 0;
+  let aim = 0;
+  if (!part2) {
+    commands.map((command) => {
+      switch (command[0]) {
+        case "forward":
+          horizontalPos = horizontalPos + command[1];
+          break;
+        case "up":
+          depth = depth - command[1];
+          break;
+        case "down":
+          depth = depth + command[1];
+          break;
+        default:
+          break;
+      }
+    });
+  } else {
+    commands.map((command) => {
+      switch (command[0]) {
+        case "forward":
+          horizontalPos = horizontalPos + command[1];
+          depth += aim * command[1];
+          break;
+        case "up":
+          aim = aim - command[1];
+          break;
+        case "down":
+          aim = aim + command[1];
+          break;
+        default:
+          break;
+      }
+    });
+  }
+  console.log("depth: " + depth);
+  console.log("horizontalPos: " + horizontalPos);
+  console.log("aim: " + aim);
 
-      default:
-        break;
-    }
-  });
+  return depth * horizontalPos;
 };
-moveAroundSubmarine(commandsArray);
-console.log(`${depth * horizontalPos} is the magic number`);
+let magicNumber = moveAroundSubmarine(commandsArray);
+let magicNumber2 = moveAroundSubmarine(commandsArray, (part2 = true));
+console.log(`${magicNumber} is the magic number`);
+console.log(`${magicNumber2} is the magic number`);
